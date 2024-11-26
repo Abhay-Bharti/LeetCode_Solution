ef6e73900f70ce12bcebcebf9c81fc42b2b4849f
Abhay Bharti <abhaybharti2123@gmail.com>
2024-11-26 23:29:16 +0530

    2816 optimized

--- 2816_Double_a_Number_Represented_as_a_Linked_List.java
+++ 2816_Double_a_Number_Represented_as_a_Linked_List.java
@@ -1,52 +1,46 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode doubleIt(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
+        int carry = 0;
+        ListNode prev = reverse(head);
+        ListNode temp = prev;
+        ListNode pre = temp;
 
-        while(curr!=null){
+        while (temp != null) {
+            int val = temp.val * 2 + carry;
+            carry = val / 10;
+            temp.val = val % 10;
+            pre = temp;
+            temp = temp.next;
+        }
+
+        if (carry > 0) {
+            ListNode newNode = new ListNode(carry);
+            pre.next = newNode;
+        }
+
+        return reverse(prev);
+    }
+
+    public static ListNode reverse(ListNode curr) {
+        ListNode prev = null;
+
+        while (curr != null) {
             ListNode n = curr.next;
             curr.next = prev;
             prev = curr;
             curr = n;
         }
 
-        int carry = 0;
-        ListNode temp = prev;
-        ListNode pre = temp;
-        
-        while(temp!=null){
-            int val = temp.val*2 + carry;
-            carry = val/10;
-            temp.val = val%10;
-            pre = temp;
-            temp = temp.next;
-        }
-
-        if(carry > 0){
-            ListNode newNode = new ListNode(carry);
-            pre.next = newNode;
-        }
-
-        ListNode newprev = null;
-        ListNode newCurr = prev;
-
-        while(newCurr!=null){
-            ListNode n = newCurr.next;
-            newCurr.next = newprev;
-            newprev = newCurr;
-            newCurr = n;
-        }
-
-        return newprev;
+        return prev;
     }
 }
