7667e68976ea07c5747fd537ccf92238b26436c0
Abhay Bharti <abhaybharti2123@gmail.com>
2025-01-15 19:30:52 +0530

    367

---
+++ 367_Valid_Perfect_Square.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+
+        int s = 0, e = num;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            long square = (long) mid * mid;
+            if (square == num) {
+                return true;
+            } else if (square > num) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return false;
+    }
+}
