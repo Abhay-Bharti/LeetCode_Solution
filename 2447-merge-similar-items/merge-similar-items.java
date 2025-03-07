7a3e794ea2d292be1b9260499c046ecaf7b66fab
Abhay Bharti <117109756+Abhay-Bharti@users.noreply.github.com>
2025-03-07 23:01:12 +0530

    Time: 12 ms (14.78%) | Memory: 45.6 MB (73.09%) - LeetSync

---
+++ 2447-merge-similar-items/merge-similar-items.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        Arrays.sort(items1, Comparator.comparingDouble(o -> o[0]));
+        Arrays.sort(items2, Comparator.comparingDouble(o -> o[0]));
+
+        int a = 0, b = 0;
+        while(a < items1.length && b < items2.length){
+            
+            if(items1[a][0] == items2[b][0]){
+                res.add(new ArrayList<>(Arrays.asList(items1[a][0] , items1[a][1]+items2[b][1])));
+                a++;
+                b++;
+            }else if(items1[a][0] < items2[b][0]){
+                res.add(new ArrayList<>(Arrays.asList(items1[a][0], items1[a][1])));
+                a++;
+            }else{
+                res.add(new ArrayList<>(Arrays.asList(items2[b][0], items2[b][1])));
+                b++;
+            }
+        }
+
+        while(a<items1.length){
+                res.add(new ArrayList<>(Arrays.asList(items1[a][0], items1[a][1])));
+                a++;
+        }
+        while(b<items2.length){
+                res.add(new ArrayList<>(Arrays.asList(items2[b][0], items2[b][1])));
+                b++;
+        }
+        return res;
+    }
+}
