85e856a077747bd8de785acb617b6138b0df988e
Abhay Bharti <117109756+Abhay-Bharti@users.noreply.github.com>
2025-03-13 12:27:38 +0530

    Time: 35 ms (66.81%) | Memory: 55.5 MB (44.95%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -2,12 +2,6 @@ class Trie {
     class Node{
         Node[] children = new Node[26];
         boolean eow;
-
-        public Node(){
-            for(int i = 0; i<26; i++){
-                children[i] = null;
-            }
-        }
     }
 
     private Node root;
