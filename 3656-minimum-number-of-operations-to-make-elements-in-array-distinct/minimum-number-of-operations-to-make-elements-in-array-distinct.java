06a096a448430d133f4a83f5ecf50da51fbdf300
Abhay Bharti <117109756+Abhay-Bharti@users.noreply.github.com>
2025-04-08 11:53:29 +0530

    Time: 3 ms (37.26%) | Memory: 44.1 MB (84.84%) - LeetSync

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int[] arr = new int[101];
+
+        int i = nums.length-1;
+        while( i >= 0){
+            if(arr[nums[i]] != 0){
+                break;
+            }else{
+                arr[nums[i]]++;
+            }
+            i--;
+        }
+
+        if(i == -1){
+            return 0;
+        }
+
+        System.out.println(i);
+        return (i+1) % 3 == 0 ? (i+1)/3 : (i+1)/3 + 1;
+
+    }
+}
