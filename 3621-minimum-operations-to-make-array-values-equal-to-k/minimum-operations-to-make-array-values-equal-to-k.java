b866fe5670b3a2e644111d06206b9aa0a87fe8ad
Abhay Bharti <117109756+Abhay-Bharti@users.noreply.github.com>
2025-04-09 10:51:06 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (95.93%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
@@ -1,12 +1,12 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        int[] val = new int[101];
+        boolean[] val = new boolean[101];
         int count = 0;
 
         for(int i = 0; i<nums.length; i++){
-            if(val[nums[i]] == 0 && nums[i] > k){
+            if(!val[nums[i]] && nums[i] > k){
                 count++;
-                val[nums[i]]++;
+                val[nums[i]] = true;
             }else if(nums[i] < k){
                 return -1;
             }
