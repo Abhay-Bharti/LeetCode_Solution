5e3657159c0e01c1cf1bdb9391cd74b605054643
Abhay Bharti <117109756+Abhay-Bharti@users.noreply.github.com>
2025-08-12 00:14:11 +0530

    Time: 5 ms (79.27%) | Memory: 42.1 MB (80.22%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,16 +1,16 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        int[] freq = new int[26];
 
-        for(int i = s.length()-1; i>=0 ; i--){
-            map.putIfAbsent(s.charAt(i), i);        
+        for(int i = 0; i<s.length(); i++){
+            freq[s.charAt(i)-'a'] = i;
         }
 
         List<Integer> list = new ArrayList<>();
         int temp = 0, last = -1;
 
         for(int i = 0; i<s.length(); i++){
-            temp = temp > map.get(s.charAt(i)) ? temp : map.get(s.charAt(i));
+            temp = temp > freq[s.charAt(i)-'a'] ? temp : freq[s.charAt(i)-'a'];
             if(temp == i){
                 list.add( i - last);
                 last = i;
