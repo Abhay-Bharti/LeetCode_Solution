665da6ed3cde694b6291f404332f95b9444b289c
Abhay Bharti <117109756+Abhay-Bharti@users.noreply.github.com>
2025-09-19 23:07:01 +0530

    Time: 108 ms (91.55%) | Memory: 56.3 MB (32.75%) - LeetSync

--- 3797-design-spreadsheet/design-spreadsheet.java
+++ 3797-design-spreadsheet/design-spreadsheet.java
@@ -2,7 +2,7 @@ class Spreadsheet {
     int[][] sheet;
 
     public Spreadsheet(int rows) {
-        this.sheet = new int[26][rows+1];
+        this.sheet = new int[26][rows + 1];
     }
 
     public void setCell(String cell, int value) {
@@ -16,32 +16,23 @@ class Spreadsheet {
     }
 
     public int getValue(String formula) {
-        int a = 0, b = 0;
-        StringBuilder sb = new StringBuilder();
+        int plus = formula.indexOf("+"), a = 0, b = 0;
+        String first = formula.substring(1, plus);
+        String second = formula.substring(plus + 1);
 
-        for (char c : formula.toCharArray()) {
-            if (c == '=')
-                continue;
-            if (c == '+') {
-                if (sb.charAt(0) <= '9')
-                    a = Integer.parseInt(sb.toString());
-                else {
-                    int[] pos = getCell(sb.toString());
-                    a = sheet[pos[0]][pos[1]];
-                }
-                sb.setLength(0);
-                continue;
-            }
-            sb.append(c);
+        if (first.charAt(0) <= '9')
+            a = Integer.parseInt(first);
+        else {
+            int[] pos = getCell(first);
+            a = sheet[pos[0]][pos[1]];
         }
 
-        if (sb.charAt(0) <= '9')
-            b = Integer.parseInt(sb.toString());
+        if (second.charAt(0) <= '9')
+            b = Integer.parseInt(second);
         else {
-            int[] pos = getCell(sb.toString());
+            int[] pos = getCell(second);
             b = sheet[pos[0]][pos[1]];
         }
-
         return a + b;
     }
 
