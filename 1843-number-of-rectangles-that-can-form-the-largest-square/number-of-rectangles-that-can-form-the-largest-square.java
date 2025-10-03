3f7079a9fb090b3515bfe64c08cfbfd630969aae
Abhay Bharti <117109756+Abhay-Bharti@users.noreply.github.com>
2025-10-03 22:50:56 +0530

    Time: 2 ms (98.80%) | Memory: 45 MB (62.40%) - LeetSync

--- 1843-number-of-rectangles-that-can-form-the-largest-square/number-of-rectangles-that-can-form-the-largest-square.java
+++ 1843-number-of-rectangles-that-can-form-the-largest-square/number-of-rectangles-that-can-form-the-largest-square.java
@@ -1,16 +1,16 @@
 class Solution {
     public int countGoodRectangles(int[][] rectangles) {
-        int n = rectangles.length, maxLen = Integer.MIN_VALUE;
+        int maxLen = Integer.MIN_VALUE, count = 0;
 
-        int[] len = new int[n];
-        for(int i = 0; i<n; i++){
-            len[i] = Math.min(rectangles[i][0], rectangles[i][1]);
-            maxLen = Math.max(maxLen, len[i]);
-        }
+        for (int i = 0; i < rectangles.length; i++) {
+            int side = Math.min(rectangles[i][0], rectangles[i][1]);
 
-        int count = 0;
-        for(int i = 0; i<n; i++){
-            if(len[i] == maxLen) count++;
+            if (side > maxLen) {
+                maxLen = side;
+                count = 1;
+            } else if(side == maxLen) {
+                count++;
+            }
         }
 
         return count;
