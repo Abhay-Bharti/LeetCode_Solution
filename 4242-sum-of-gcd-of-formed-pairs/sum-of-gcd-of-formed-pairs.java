1069a72c6de5ce26efae6a682c98a3ff0c005025
Abhay Bharti <117109756+Abhay-Bharti@users.noreply.github.com>
2026-07-16 22:30:00 +0530

    Time: 53 ms (95.50%) | Memory: 108 MB (63.50%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
@@ -10,14 +10,11 @@ class Solution {
         for (int i = 1; i < n; i++) {
             max = nums[i] < max ? max : nums[i];
             arr[i] = gcd(max, nums[i]);
-            // System.out.printf("%d, %d\n", max, arr[i]);
         }
         
         Arrays.sort(arr);
-        // System.out.println();
         for(int i = 0; i < n/2; i++){
             sum += (long) gcd(arr[i], arr[n-i-1]);
-            // System.out.println(sum);
         }
 
         return sum;
